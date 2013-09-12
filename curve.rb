8d607d9d6142b4c935611e881e79ee97b97bf14b
ottony <ottony.costa@hotmail.com>
2013-09-12 18:20:44 -0300

    Defined one generic "cicloide" math whit two bolts (Internal circle @R and external circle @r) and draw it

--- curve.rb
+++ curve.rb
@@ -1,19 +1,33 @@
 require 'gosu'
+require './point'
 
 class Curve
+  attr_accessor :win
+  
   def initialize(window)
-    @w =  window
+    @win =  window
     @t = 0
-    @angles = []
+    @points = []
+    @R = 100
+    @r = 10
+    @image = Gosu::Image.new(@win, 'point_image.png', true)
+
   end
 
   def update
-    @t += 1
-    @angles <<  @t
+    if (@t < Math::PI*2)
+      @t += 0.01
+    
+      @x = (@R+@r)*Math.cos(@t) + @r*Math.sin(@t*(@R+@r)/@r - (Math::PI)/2)
+      @y = (@R+@r)*Math.sin(@t) - @r*Math.cos(@t*(@R+@r)/@r - (Math::PI)/2)
+
+      @points << Point.new(@image, @x, @y)
+    end
   end
 
   def draw
-    @angles.each do |a|
+    @points.each do |point|
+      point.image.draw(point.x + 450,point.y + 350,0, 0.1, 0.1)
       #Gosu::Font.new(@w, "Times New Roman", 20).draw("Index: " + a.to_s,0,0,0)
     end
   end
