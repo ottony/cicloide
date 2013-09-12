5410c00b97ecf53a7cf01d42f31e4e56c084fd7e
ottony <ottony.costa@hotmail.com>
2013-09-12 19:26:27 -0300

    Draw lines to simule vectors

--- curve.rb
+++ curve.rb
@@ -9,15 +9,16 @@ class Curve
     @t = 0
     @points = []
     @R = 100
-    @r = 10
+    @r = 100
+    
     @image = Gosu::Image.new(@win, 'point_image.png', true)
 
   end
 
   def update
     if (@t < Math::PI*2)
-      @t += 0.01
-    
+      @t += Math::PI/80
+      
       @x = (@R+@r)*Math.cos(@t) + @r*Math.sin(@t*(@R+@r)/@r - (Math::PI)/2)
       @y = (@R+@r)*Math.sin(@t) - @r*Math.cos(@t*(@R+@r)/@r - (Math::PI)/2)
 
@@ -29,6 +30,7 @@ class Curve
     @points.each do |point|
       point.image.draw(point.x + 450,point.y + 350,0, 0.1, 0.1)
       #Gosu::Font.new(@w, "Times New Roman", 20).draw("Index: " + a.to_s,0,0,0)
+      @win.draw_line(450, 350, Gosu::Color::BLACK, point.x + 450, point.y + 350, Gosu::Color::BLUE)
     end
   end
 end
