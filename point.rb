637f7deecfd76f1885e4cbae6b78dd6f11473f3b
ottony <ottony.costa@hotmail.com>
2013-09-12 18:19:01 -0300

    Created the params of point object

--- point.rb
+++ point.rb
@@ -1,4 +1,11 @@
 class Point
   attr_accessor :x
   attr_accessor :y
+  attr_reader :image
+  
+  def initialize (image_, x_ = 0, y_ = 0)
+      @x =  x_
+      @y =  y_
+      @image = image_
+  end
 end
