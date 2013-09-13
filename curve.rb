0bc475d193d4f1ba099ab7981924fe32750c9892
ottony <ottony.costa@hotmail.com>
2013-09-13 07:04:24 -0300

    Defined uno sequence of draw
    0<=t<=2PI Draw vectors
    2PI<=t<=4PI Draw point (whit angle more slow)
    t = angle

--- curve.rb
+++ curve.rb
@@ -1,5 +1,5 @@
 require 'gosu'
-require './point'
+require './vector'
 
 class Curve
   attr_accessor :win
@@ -7,30 +7,47 @@ class Curve
   def initialize(window)
     @win =  window
     @t = 0
+    @count_vector = 0;
     @points = []
+    @vectors = []
     @R = 100
     @r = 100
     
-    @image = Gosu::Image.new(@win, 'point_image.png', true)
+    @point_img = Gosu::Image.new(@win, 'point_image.png', true)
+    @vector_img = Gosu::Image.new(@win, 'vector_image.png', true)
 
   end
 
   def update
-    if (@t < Math::PI*2)
+    if (@t <= Math::PI*2)
       @t += Math::PI/80
-      
-      @x = (@R+@r)*Math.cos(@t) + @r*Math.sin(@t*(@R+@r)/@r - (Math::PI)/2)
-      @y = (@R+@r)*Math.sin(@t) - @r*Math.cos(@t*(@R+@r)/@r - (Math::PI)/2)
-
-      @points << Point.new(@image, @x, @y)
+      @points << get_point(@t)
+      @count_vector += 1
+    elsif (@t<= Math::PI*4)
+      @t += Math::PI/600
+      @points << get_point(@t)
     end
   end
 
+  def get_point(angle)
+      Point.new(@point_img, x_position(angle), y_position(angle))
+  end
+
+  def x_position(angle)
+     (@R+@r)*Math.cos(angle) + @r*Math.sin(angle*(@R+@r)/@r - (Math::PI)/2)
+  end
+
+  def y_position(angle)
+      (@R+@r)*Math.sin(angle) - @r*Math.cos(angle*(@R+@r)/@r - (Math::PI)/2)
+  end
+
   def draw
-    @points.each do |point|
-      point.image.draw(point.x + 450,point.y + 350,0, 0.1, 0.1)
-      #Gosu::Font.new(@w, "Times New Roman", 20).draw("Index: " + a.to_s,0,0,0)
-      @win.draw_line(450, 350, Gosu::Color::BLACK, point.x + 450, point.y + 350, Gosu::Color::BLUE)
+    @points.each_with_index do |point, i|
+      if (i > @count_vector)
+        point.image.draw(point.x + 450,point.y + 350,0, 0.1, 0.1)
+      else 
+        @win.draw_line(450, 350, Gosu::Color::BLACK, point.x + 450, point.y + 350, Gosu::Color::BLUE)
+      end
     end
   end
 end
