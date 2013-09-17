71a353ff47da2ce3945bae5974d7df6126f5adbc
ottony <ottony.costa@hotmail.com>
2013-09-17 17:58:08 -0300

    Crete equation class to operate whit equations

--- models/equation.rb
+++ models/equation.rb
@@ -1,5 +1,25 @@
+require 'gosu'
 require './models/point'
 
 class Equation
- 
+
+  def initialize(window)
+    @point_img = Gosu::Image.new(window, './media/point_image.png', true)
+    @@R = 100
+    @@r = 100
+  end 
+
+  def get_point(angle)
+      Point.new(@point_img, x_position(angle), y_position(angle))
+  end
+
+  protected
+  
+  def x_position(angle)
+     (@@R+@@r)*Math.cos(angle) + @@r*Math.sin(angle*(@@R+@@r)/@@r - (Math::PI)/2)
+  end
+
+  def y_position(angle)
+      (@@R+@@r)*Math.sin(angle) - @@r*Math.cos(angle*(@@R+@@r)/@@r - (Math::PI)/2)
+  end
 end
