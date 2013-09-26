f9f52ef965f49eece456f2275b2c1d72ef14b773
ottony <ottony.costa@hotmail.com>
2013-09-26 18:43:33 -0300

    creates method to update number of entrace in graphic

--- models/curve.rb
+++ models/curve.rb
@@ -10,9 +10,49 @@ class Curve
     @points = []
     @vectors = []
     @equation = equation
-    @text = Gosu::Font.new(@win, "Times New Roman", 30)
-
   end
+  
+  def reload
+    @points.clear
+    @vectors.clear
+    @t = 0
+    @count_vector = 0;
+    @points = []
+    @vectors = []
+  end
+
+  def setCav(rason)
+    case rason
+    when 1
+      @equation.R = 1*100
+      @equation.r = 100
+    when 2
+      @equation.R = 2*5*10
+      @equation.r = 5*10
+    when 3
+      @equation.R = 3*3*10
+      @equation.r = 3*10
+    when 4
+      @equation.R = 4*5*5
+      @equation.r = 5*5
+    when 5
+      @equation.R = 5*5*5
+      @equation.r = 5*5
+    when 6
+      @equation.R = 6*5*5
+      @equation.r = 5*5
+    when 7
+      @equation.R = 7*3*10
+      @equation.r = 3*10
+    when 8
+      @equation.R = 8*5*5
+      @equation.r = 5*5
+    when 9
+      @equation.R = 9*3*8
+      @equation.r = 3*8
+    end
+  end
+
 
   def update
     if (@t <= Math::PI*2)
@@ -27,10 +67,9 @@ class Curve
 
 
   def draw
-   # @text.draw("CTRL+SHIFT+r set R | CTRL+r set r ", 10,10,0)
     @points.each_with_index do |point, i|
       if (i > @count_vector)
-        point.image.draw(point.x + 450,point.y + 350,0, 0.1, 0.1)
+        point.image.draw(point.x + 450,point.y + 350,0, 0.08, 0.08)
       else 
         @win.draw_line(450, 350, Gosu::Color::BLACK, point.x + 450, point.y + 350, Gosu::Color::BLUE)
       end
