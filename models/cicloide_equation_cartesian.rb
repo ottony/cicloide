903ffa84c771924bcce96c7026859399968ecf79
ottony <ottony.costa@hotmail.com>
2013-10-01 02:41:32 -0300

    create method to return string of equation.

--- models/cicloide_equation_cartesian.rb
+++ models/cicloide_equation_cartesian.rb
@@ -1,23 +1,28 @@
 require 'gosu'
 require './models/point'
 
-class Equation
-  attr_accessor :R, :r
+class CicloideEquationCartesian
+  attr_accessor :R, :r, :hipo
 
   def initialize(window)
     @point_img = Gosu::Image.new(window, './media/point_image.png', true)
     @R = 100
     @r = 100
+    @hipo = 1
   end 
 
-  def get_point(angle)
+  def getPoint(angle)
       Point.new(@point_img, x_position(angle), y_position(angle))
+  end
+
+  def getEquationString
+    ["X = (R+r)*cos(t) + r*sin((t/r)*(R+r) - PI/2)", "Y = (R+r)*sin(t) + r*cos((t/r)*(R+r) - PI/2)"]
   end
 
   protected
   
   def x_position(angle)
-     (@R+@r)*Math.cos(angle) + @r*Math.sin(angle*(@R+@r)/@r - (Math::PI)/2)
+     (@R+@r)*Math.cos(angle) + @hipo*@r*Math.sin(angle*(@R+@r)/@r - (Math::PI)/2)
   end
 
   def y_position(angle)
